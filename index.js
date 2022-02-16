f6491ccacb3852e7a26da5a69c37deb29f238f46
arjun <ameena@tataunistore.com>
2022-02-16 19:18:54 +0530

    Connected to Database

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv/config')
 
+
+
+//Setting up the express server
 const app = express();
-
 const PORT = 8080;
 
+//Connecting to Database
+mongoose.connect(process.env.DB_CONNECTION,()=>{
+    console.log("Connected to Database Successfully!")
+})
+
+//Routes
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
